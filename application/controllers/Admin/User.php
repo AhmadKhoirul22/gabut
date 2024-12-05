d49fb3b5fc424506c01a41b8da54a4dcdbd1a358
AhmadKhoirul22 <slowmohuda@gmail.com>
2024-12-05 12:32:25 +0700

    Update User.php

--- application/controllers/Admin/User.php
+++ application/controllers/Admin/User.php
@@ -4,6 +4,9 @@ class User extends CI_Controller{
 		parent::__construct();
 		$this->load->library('template');
 		$this->load->model('User_model');
+		if($this->session->userdata('id_user') == null ){
+			redirect('auth');
+		}
 	}
 	public function index(){
 		$data['title'] = 'User';
