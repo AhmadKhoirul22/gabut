2d25d7b7f69329591f0f5a4948605096f5d01f45
AhmadKhoirul22 <slowmohuda@gmail.com>
2025-01-29 21:54:55 +0700

    Update template.php

--- application/views/admin/template.php
+++ application/views/admin/template.php
@@ -32,13 +32,6 @@
   <!-- Template Main CSS File -->
   <link href="<?= base_url('assets/niceadmin/') ?>assets/css/style.css" rel="stylesheet">
 
-  <!-- =======================================================
-  * Template Name: NiceAdmin
-  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-  * Updated: Apr 20 2024 with Bootstrap v5.3.3
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
 </head>
 
 <body>
